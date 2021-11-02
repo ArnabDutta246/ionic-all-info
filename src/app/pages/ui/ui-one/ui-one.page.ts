7b378d478b9e726e4812f1e375f196a95383dc79
ArnabDutta246 <arnab2461997@gmail.com>
2021-11-02 14:12:29 +0530

    bottom card content with animation

--- src/app/pages/ui/ui-one/ui-one.page.ts
+++ src/app/pages/ui/ui-one/ui-one.page.ts
@@ -1,11 +1,12 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 @Component({
   selector: 'app-ui-one',
   templateUrl: './ui-one.page.html',
   styleUrls: ['./ui-one.page.scss'],
 })
 export class UiOnePage implements OnInit {
+  @ViewChild(IonContent,{read:ElementRef,static:true}) contentArea:ElementRef;
 @ViewChild('triggerElements',{read:ElementRef,static:true}) triggerElemnets:ElementRef;
   private observer: IntersectionObserver;
   // data object
@@ -17,14 +18,33 @@ export class UiOnePage implements OnInit {
     desc:"",
     sizes:"",
   }
-  constructor() { }
+  constructor(private renderer:Renderer2) { }
 
   ngOnInit() {
+
+    this.callTheObserver();
   }
   handleEvent(ev){
     console.log('this ion content is scrolling',ev)
   }
 
   // using interception observer
+  callTheObserver(){
+    this.observer = new IntersectionObserver(entries=>{
+      entries.forEach((entry:any)=>{
+        if(entry.isIntersecting){
+          // do ode
+          console.log("trigger elements add transform");
+          this.renderer.addClass(this.contentArea.nativeElement, "no-transform");
+        }else{
+          // do if not
+          console.log("remove transform");
+          this.renderer.removeClass(this.contentArea.nativeElement,"no-transform");
+        }
+      })
+    });
 
+    // another
+    this.observer.observe(this.triggerElemnets.nativeElement);
+  }
 }
