dd274b5d270cbbf3aae9cad6cfb7595549440d3f
ArnabDutta246 <arnab2461997@gmail.com>
2021-11-02 15:36:35 +0530

    swiper add

--- src/app/pages/ui/swiperjs/swiperjs.page.ts
+++ src/app/pages/ui/swiperjs/swiperjs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper, { Pagination, SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-swiperjs',
@@ -6,12 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swiperjs.page.scss'],
 })
 export class SwiperjsPage implements OnInit {
-  public swiperConfig:any = {};
+  public swiperConfig:SwiperOptions = {
+    slidesPerView: 2, // how much item will show in single time
+    spaceBetween: 50, // space between them
+    mousewheel: true, // navigation are allowed
+    scrollbar: { draggable: true }, // can scroll able
+    pagination:{clickable:true},  // bottom pagination indicator are allowed
+  };
   constructor() { }
 
   ngOnInit() {
-
-
+  // init swiper 
+   Swiper.use([Pagination]);
   }
 
+
+
+  //======= Reference 
+  // https://swiperjs.com/angular
+  // Documentation are available in this page
 }
